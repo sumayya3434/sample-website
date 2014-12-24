313f153afa9d659913d9644e5a1a2f20d2f1d415
sumayya <sumayya@qburst.com>
2014-12-24 13:47:42 +0530

    added files of speakeasy.com

---
+++ js/sideLink.js
@@ -0,0 +1,58 @@
+
+function enlarge(x) 
+{
+	x.style.width = "250px";
+}
+function normal(x) 
+{
+    x.style.height = "50px";
+    x.style.width = "200px";
+
+}
+function formValidation()
+{
+    var name = document.regform.user;
+    var telno = document.regform.telno;
+    if(usernameValidation(name,5,12))
+    {
+    	if(telValidation(tel))
+        {
+        	
+    	 return true;
+        }
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function usernameValidation(name,lowlimit,uplimit)  
+{  
+	var namelen = name.value.length;  
+	if (namelen == 0 || namelen >= uplimit || namelen < lowlimit)  
+	{  
+		alert("Username should not be empty and length be between "+lowlimit+" to "+uplimit);  
+		name.focus();  
+		return false;  
+	}  
+  
+		return true;
+}
+function telValidation(telno)
+{
+
+	var numlen = telno.value.length;
+	
+	var ten = 10;
+	alert(ten);
+	if(numlen == 0 || numlen > ten)
+	{
+		 
+		alert("phone number must not be empty and length should be less than 10");
+        tel.focus();
+        return false;
+	}
+	return true;
+}
+
